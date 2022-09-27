0e124b7eb4c9af3e9ecb7b9e298f85fb539433fc
Ray <32885937+Rui1521@users.noreply.github.com>
2022-09-27 13:40:22 -0700

    Update models.py

--- models.py
+++ models.py
@@ -2,8 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch.optim.optimizer import Optimizer, required
-from torch import Tensor
+from torch.optim.optimizer import Optimizer
 from torch.nn import Parameter
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
